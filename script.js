605ec6f1106427563dcbe915c6fd2ab5d57266c0
Mels <194421646+melturnsup@users.noreply.github.com>
2025-05-21 16:02:05 +0100

    Javascript code finalized and tested - generates a random positive message

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
+/*A function that returns a random message*/
 
+const randMsg = () => {
 
-//Array of different messages
-const randMsg = ["Nothing is impossible. The word itself says i'm possible!", "Good luck today!", "Mistakes are part of the learning process", "The world is better because I am in it", "I trust my intuition."];
-/*A function that loops through the index of the 
-positive message array and prints a random string*/
+  //Array of different positive messages
+  const randMsgArr = [
+  "Nothing is impossible. The word itself says i'm possible!",
+  "Good luck today!", 
+  "Mistakes are part of the learning process!",
+  "The world is better because I am in it!",
+  "I trust my intuition!"
+  ];
 
-let randInt = () => {
-  return Math.floor(Math.random() * (5 - 1 + 1) + 1);
-}
+  //Generates a random intiger between 0 and 4, the amount of items in the array.
+  let randInt = () => {
+  return Math.floor(Math.random() * randMsgArr.length);
+  };
 
-console.log(randInt());
+  /* Test of random intiger generator
+  console.log(randInt());
+  */
+
+  //connecting the random number generator as the index of the array
+  const index = randInt();
+  result = randMsgArr[index];
+  console.log(result);
+  return result;
+  
+};
+
+/* final test
+randMsg();
+*/
